ef3c7ee5b3f3873e6a6f7814ac62738133c9a291
BlizzardFenrir <jan.veldthuis@gmail.com>
2016-04-16 15:00:48 +0200

    Added moving wall.

---
+++ Assets/Scripts/WallMove.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallMove : MonoBehaviour {
+  public float speed;
+
+  void Update () {
+    transform.position += new Vector3(0.0f, 0.0f, speed) * Time.deltaTime;
+  }
+}
