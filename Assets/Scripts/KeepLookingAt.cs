f577998626295fc374be025b886c5edf1622ef24
Marten Veldthuis <marten@veldthuis.com>
2016-04-16 16:34:20 +0100

    Camera tweaks, add stage wall

---
+++ Assets/Scripts/KeepLookingAt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepLookingAt : MonoBehaviour {
+
+  public GameObject lookAt;
+
+  void Update() {
+    transform.LookAt(lookAt.transform);
+    transform.Rotate(Vector3.left * 10);
+  }
+}
