3829be3440ab14001dd383b35a912e6afd509e3d
Marten Veldthuis <marten@veldthuis.com>
2016-04-17 22:06:57 +0100

    Add applause

---
+++ Assets/Scripts/ApplaudOnWin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplaudOnWin : MonoBehaviour {
+
+  void OnEnable() {
+    WinLoseEvent.OnWinStage += Applaud;
+  }
+
+  void OnDisable() {
+    WinLoseEvent.OnWinStage -= Applaud;
+  }
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+
+  void Applaud() {
+    GetComponent<AudioSource>().Play();
+  }
+    
+}
