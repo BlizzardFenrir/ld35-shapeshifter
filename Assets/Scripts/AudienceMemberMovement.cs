e2ccb46f1d508235d85e775218478ef512b6c6af
Marten Veldthuis <marten@veldthuis.com>
2016-04-17 17:25:36 +0100

    shifty audience members

---
+++ Assets/Scripts/AudienceMemberMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudienceMemberMovement : MonoBehaviour {
+  public float shiftAmount = 0.2f;
+  public double shiftChance = 0.05;
+
+  private System.Random rng;
+  private bool shiftedUp = false;
+  private bool shiftedLeft = false;
+  private bool shiftedRight = false;
+  private Vector3 initialPosition;
+
+	// Use this for initialization
+	void Start () {
+    rng = new System.Random();
+    initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    RandomizePosition();
+    Debug.Log(shiftedUp);
+    transform.position = initialPosition;
+
+    transform.position = new Vector3(
+      initialPosition.x,
+      initialPosition.y + (shiftedUp ? shiftAmount : 0.0f),
+      initialPosition.z + (shiftedRight ? shiftAmount : 0.0f) + (shiftedLeft ? -shiftAmount : 0.0f)
+    );
+	}
+
+  void RandomizePosition() {
+    if (rng.NextDouble() < shiftChance) { 
+      shiftedUp = !shiftedUp;
+    }
+    if (2*rng.NextDouble() < shiftChance) { 
+      shiftedLeft = !shiftedLeft;
+    }
+    if (2*rng.NextDouble() < shiftChance) { 
+      shiftedRight = !shiftedRight;
+    }
+  }
+}
