c3c42a1862978659b27d85a6a30fdf2537e5b408
BlizzardFenrir <jan.veldthuis@gmail.com>
2016-04-17 14:12:53 +0200

    Added blacklist for random shapes.

---
+++ Assets/Scripts/RandomBlink.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomBlink : MonoBehaviour {
+
+  public GameObject blinkObject;
+  private List<Vector3> blinkOffsets;
+
+  public float nextBlinkTime;
+
+  [HideInInspector][System.NonSerialized]
+  private Mesh mesh;
+
+  // Use this for initialization
+  void Awake() {
+    mesh = GetComponent<MeshFilter>().mesh;
+  }
+
+  void Start () {
+    blinkOffsets = new List<Vector3>();
+
+    Mesh blinkMesh = blinkObject.GetComponent<MeshFilter>().sharedMesh;
+    Vector3[] transformVertices = blinkMesh.vertices;
+    Vector3[] vertices = mesh.vertices;
+
+    for (int i = 0; i < vertices.Length; i++) {
+     blinkOffsets.Add( transformVertices[i] - vertices[i] );
+    }
+
+    nextBlinkTime = Time.time + Random.Range(1.0f, 10.0f);
+ }
+
+  // Update is called once per frame
+  void Update () {
+    float animationLength = 0.5f;
+
+    if (Time.time > nextBlinkTime) {
+      float deltaTime = nextBlinkTime - Time.time;
+      float eased = EasingFunctions.EaseOutElastic(deltaTime, 0, 1, animationLength);
+      float scalefactor = Mathf.Clamp(eased, 0.0f, 1.0f);
+
+      Vector3[] vertices = GetComponent<MeshFilter>().mesh.vertices;
+      for (int i = 0; i < vertices.Length; i++) {
+        vertices[i] += blinkOffsets[i] * scalefactor;
+      }
+
+      mesh.vertices = vertices;
+      mesh.RecalculateBounds();
+    }
+  }
+}
--- Assets/Scripts/RandomKeypresses.cs
+++ Assets/Scripts/RandomKeypresses.cs
@@ -7,10 +7,13 @@ using System.Collections.Generic;
 public class RandomKeypresses : TweenKeypresses {
 
     public int difficulty;
+    public List< string > forbiddenCombinations;
     private Dictionary<char, bool> states;
+    System.Random rand;
 
     void Awake () {
       states = new Dictionary<char, bool>();
+      rand = new System.Random();
 
       VertexTransform vt = GetComponent<VertexTransform>();
       List<char> keys = new List<char>();
@@ -25,18 +28,40 @@ public class RandomKeypresses : TweenKeypresses {
     }
 
     public void Randomize() {
-      // Reset
-      List<char> keys = new List<char>(states.Keys);
-      foreach (char key in keys) {
-        states[key] = false;
+      int randomizationAttempts = 3;
+
+      while (randomizationAttempts > 0) {
+        // Reset
+        List<char> keys = new List<char>(states.Keys);
+        foreach (char key in keys) {
+          states[key] = false;
+        }
+
+        // Randomize
+        List<char> pressedKeys = keys.OrderBy(c => rand.Next()).Take(difficulty).ToList();
+        foreach (char k in pressedKeys) {
+          states[k] = true;
+        }
+        // Debug.Log("Attempts: " + randomizationAttempts + ", " + getShapeLetters() );
+
+        // Verify forbidden combinations
+        bool passedTests = true;
+        foreach (string combination in forbiddenCombinations) {
+          if (!combination.Except(pressedKeys).Any()) {
+            // Matched a forbidden combination
+            // Debug.Log("Found forbidden combination: " + combination);
+            passedTests = false;
+          }
+        }
+        if (!passedTests) {
+          randomizationAttempts -= 1;
+        } else {
+          randomizationAttempts = 0;
+        }
       }
 
-      // Randomize
-      System.Random rand = new System.Random();
-      List<char> pressedKeys = keys.OrderBy(c => rand.Next()).Take(difficulty).ToList();
-      foreach (char k in pressedKeys) {
-        states[k] = true;
-      }
+      // Correctly randomized
+      // Debug.Log("Good randomization: " + getShapeLetters() );
     }
     
     void Update () {
