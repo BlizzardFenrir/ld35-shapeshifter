b5b9372e9495f10a1a2cb5715c3853f878eec484
Marten Veldthuis <marten@veldthuis.com>
2016-04-17 11:50:49 +0100

    Smooth camera

--- Assets/Scripts/KeepLookingAt.cs
+++ Assets/Scripts/KeepLookingAt.cs
@@ -3,10 +3,23 @@ using System.Collections;
 
 public class KeepLookingAt : MonoBehaviour {
 
+  public Vector3 desiredPosition;
+  private Vector3 currentVelocity;
+  public float wallPlace = 0.0f;
+  public float dampTime = 0.2f;
+  private Vector3 initialPosition;
+
   public GameObject lookAt;
 
+  void Start() { 
+    initialPosition = transform.position;
+    desiredPosition = initialPosition;
+  }
+
   void Update() {
-    transform.LookAt(lookAt.transform);
+	  transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref currentVelocity, dampTime);
+
+	  transform.LookAt(lookAt.transform);
     transform.Rotate(Vector3.left * 10);
   }
 }
--- Assets/Scripts/WallMove.cs
+++ Assets/Scripts/WallMove.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class WallMove : MonoBehaviour {
   public float speed;
-  public Camera camera;
+  public KeepLookingAt cameraMovement;
   public GameObject wallShape;
 
   void Start() {
@@ -17,12 +17,12 @@ public class WallMove : MonoBehaviour {
     if (transform.position.z < -10) {
       ResetWall();
     }
-    camera.transform.position += translation * 0.1f;
+    cameraMovement.desiredPosition += translation * 0.1f;
   }
 
   void ResetWall() {
     wallShape.GetComponent<RandomKeypresses>().Randomize();
     transform.position = new Vector3(transform.position.x, transform.position.y, 28);
-    camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, -7.11f);
+    cameraMovement.desiredPosition = new Vector3(cameraMovement.desiredPosition.x, cameraMovement.desiredPosition.y, -7.11f);
   }
 }
