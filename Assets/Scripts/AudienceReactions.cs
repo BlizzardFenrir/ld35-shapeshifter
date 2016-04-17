fb27ea8abeac770edd66c0c9b4ccef9b04525592
BlizzardFenrir <jan.veldthuis@gmail.com>
2016-04-17 18:09:24 +0200

    Added colliders for seating.

--- Assets/Scripts/AudienceReactions.cs
+++ Assets/Scripts/AudienceReactions.cs
@@ -2,14 +2,32 @@
 using System.Collections;
 
 public class AudienceReactions : MonoBehaviour {
+  public float minTimeBetweenJumps;
+  public float maxTimeBetweenJumps;
+  public float minJumpHeight;
+  public float maxJumpHeight;
+  public float winJumpHeight;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  // private float startingHeight;
+  private float nextJumpTime;
+
+  void Start() {
+    // startingHeight = transform.position.y;
+    nextJumpTime = Time.time + Random.Range(minTimeBetweenJumps, maxTimeBetweenJumps);
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (Time.time > nextJumpTime) {
+      Jump();
+    }
+
+    // if (transform.position.z < startingHeight)
+    //   transform.position = new Vector3(transform.position.x, transform.position.y, startingHeight);
+  }
+
+  public void Jump() {
+    float jumpHeight = Random.Range(minJumpHeight, maxJumpHeight);
+
+  }
 }
