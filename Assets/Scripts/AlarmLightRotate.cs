fe8305ed6e6341380c8585c37a2b39b08271e760
Marten Veldthuis <marten@veldthuis.com>
2016-04-17 12:39:27 +0100

    Stop rotating if green

--- Assets/Scripts/AlarmLightRotate.cs
+++ Assets/Scripts/AlarmLightRotate.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class AlarmLightRotate : MonoBehaviour {
+  public bool rotating = true;
 
-	// Use this for initialization
-	void Start () {
+  void Start() {
+  }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+  void Update() {
+    if (rotating) {
       transform.Rotate(Vector3.forward * Time.deltaTime * 500, Space.World);
-	}
+    } else {
+      transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+  }
 }
--- Assets/Scripts/ChangeLightColor.cs
+++ Assets/Scripts/ChangeLightColor.cs
@@ -6,13 +6,17 @@ public class ChangeLightColor : MonoBehaviour {
   public bool state;
   public List<Light> lights;
 
+  private AlarmLightRotate rotator;
+
 	// Use this for initialization
 	void Start () {
-	
+    rotator = this.GetComponent<AlarmLightRotate> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+    rotator.rotating = !state;
+
 	  if (state) {
         foreach (var light in lights) {
           light.color = new Color(0, 1, 0);
